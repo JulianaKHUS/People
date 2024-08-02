2ea45df6f95e3091ef2b4231afcd55a0285e6db2
Juliana <iulianka99@mail.ru>
2024-08-02 21:08:50 +0300

    People

---
+++ src/Person.java
@@ -0,0 +1,68 @@
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this(name, surname);
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age > 0;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) {
+            age++;
+        }
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(surname)
+                .setAddress(address);
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + " (" + age + " years old, " + address + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() ^ surname.hashCode();
+    }
+}
+
+
